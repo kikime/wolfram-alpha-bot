58ad4c32339aab5ca89aef3aadf2ee27b89caea8
Niclas <njdm.evil@gmail.com>
2017-06-02 19:26:11 +0200

    Import api data

--- wolfram-alpha-bot.py
+++ wolfram-alpha-bot.py
@@ -1,3 +1,11 @@
+import os
+
+WA_APP = os.environ["wolframalpha_app_id"]
+CLIENT_ID = os.environ["reddit_client_id"]
+CLIENT_SECRET = os.environ["reddit_client_secret"]
+USERNAME = os.environ["reddit_username"]
+PASSWORD = os.environ["reddit_password"]
+
 def main():
     pass
 
