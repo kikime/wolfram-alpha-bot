567e223e359601b0c1c6280fdc69bf8fd8f638f1
Niclas <njdm.evil@gmail.com>
2017-06-02 19:29:59 +0200

    Now connecting to reddit ( + formating)

--- wolfram-alpha-bot.py
+++ wolfram-alpha-bot.py
@@ -1,13 +1,22 @@
 import os
 
-WA_APP = os.environ["wolframalpha_app_id"]
-CLIENT_ID = os.environ["reddit_client_id"]
+import praw
+import wolframalpha
+
+WA_APP        = os.environ["wolframalpha_app_id"]
+CLIENT_ID     = os.environ["reddit_client_id"]
 CLIENT_SECRET = os.environ["reddit_client_secret"]
-USERNAME = os.environ["reddit_username"]
-PASSWORD = os.environ["reddit_password"]
+USERNAME      = os.environ["reddit_username"]
+PASSWORD      = os.environ["reddit_password"]
 
 def main():
-    pass
+    r = praw.Reddit(
+        user_agent    = "wolfram-alpha-bot 007 (by /u/SteveCCL)",
+        client_id     = CLIENT_ID,
+        client_secret = CLIENT_SECRET,
+        username      = USERNAME,
+        password      = PASSWORD
+)
 
 if __name__ == "__main__":
     main()
