9d8b8b8844e133d4a6abadf71e25b573705ea098
Niclas <njdm.evil@gmail.com>
2017-06-02 20:04:10 +0200

    Now only checking relevant messages

--- wolfram-alpha-bot.py
+++ wolfram-alpha-bot.py
@@ -1,4 +1,6 @@
 import os
+import re
+import time
 
 import praw
 import wolframalpha
@@ -9,6 +11,8 @@ CLIENT_SECRET = os.environ["reddit_client_secret"]
 USERNAME      = os.environ["reddit_username"]
 PASSWORD      = os.environ["reddit_password"]
 
+IS_RELEVANT = re.compile(r"\s*\+?/?u/wolfram-alpha-bot\s+")
+
 def main():
     r = praw.Reddit(
         user_agent    = "wolfram-alpha-bot 007 (by /u/SteveCCL)",
@@ -16,7 +20,22 @@ def main():
         client_secret = CLIENT_SECRET,
         username      = USERNAME,
         password      = PASSWORD
-)
+    )
+    wa = wolframalpha.Client(WA_APP)
+
+    print("Bot up and running")
+
+    while 1:
+        for message in r.inbox.unread():
+            try:
+                if re.match(IS_RELEVANT, message.body):
+                    print(message.body)
+                message.mark_read()
+            except Exception as e:
+                print("An error occured. ({})".format(str(e)))
+                print("Retrying in a minute.")
+                time.sleep(60)
+
 
 if __name__ == "__main__":
     main()
